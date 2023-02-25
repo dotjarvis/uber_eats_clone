be5a7ecfe6240d5dec99a44fd97d27111ab44f27
jarvis_codes <106377265+dotjarvis@users.noreply.github.com>
2023-02-25 09:20:51 +0300

    9th commit
    Resetting the look of the site, quite a nightmare but am not giving up, Not even close

--- script/landingpage.js
+++ script/landingpage.js
@@ -1,6 +1,6 @@
 const menu_bar = document.querySelector(".bx-menu");
 const overlay = document.querySelector(".blur");
-const hidden_menu = document.querySelector(".aside-hidden-menu");
+const hidden_menu = document.querySelector(".mini-menu");
 
 menu_bar.addEventListener("click", function () {
   hidden_menu.classList.remove("hidden");
